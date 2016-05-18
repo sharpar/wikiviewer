3e2d57cc807b38e0349a0c13618109f5f8a8ec8f
sharpar <shivain27@gmail.com>
2016-05-18 19:47:44 +0400

    trying to understand middleman

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -3,10 +3,3 @@
 /*jslint browser: true*/
 /*global $, jQuery, alert*/
 /*jslint devel: true */
-
-$(document).ready(function () {
-    'use strict';
-
-    $("#search").on('click', function () {});
-
-});
